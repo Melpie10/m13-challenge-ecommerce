6af81d47d9ffeb571c1fa8e367e699be0d88a657
melany14 <melany14@gmail.com>
2022-02-12 23:11:17 -0500

    made changes to rput amd post routes on tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -52,11 +52,34 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
-
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+   Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData[0]) {
+        res.status(404).json({ message: 'No tag found with this id' });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    }); 
 });
 
 router.delete('/:id', (req, res) => {
